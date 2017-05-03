7b70b2e8275c196bb43c68d0bf8bb81da1a54e8b
freshdirect <dfuentes@freshdirect.com>
2017-05-03 10:08:23 -0400

    adding carousel to modules section. side note: for some reason, a lot of file, even ones that have not being changed

--- assets/js/vendor/script.js
+++ assets/js/vendor/script.js
@@ -2,10 +2,6 @@
 // Modules
 
 /// Overlay
-
-
-
-
 $("#sm-overlay-btn").click(function() {
 	$(".overlay-bg.sm").addClass('show');
 	$("body").addClass('noscroll');
@@ -36,5 +32,88 @@ $(document).keyup(function(e) {
     	$("body").removeClass('noscroll');
 		$("nav").removeClass('hide');	
 	}
+});
+
+/// Carousel
+
+//// functions
+checkCurrent = function(){
+
+	if ($("#t-carousel-1 .fd-carousel li:first").hasClass("current-slide")){
+
+		$(".fd-carrousel__button__previous button").attr("disabled", true);
+		$(".fd-carrousel__button__next button").attr("disabled", false);
+
+	} else if ($("#t-carousel-1 .fd-carousel li:last").hasClass("current-slide")) {
+
+		$(".fd-carrousel__button__next button").attr("disabled", true);
+		$(".fd-carrousel__button__previous button").attr("disabled", false);
+
+	} else {
+
+		$(".fd-carrousel__button-container button").attr("disabled", false);
+
+	}
+
+}
+
+focusFirstEl = function(){
+
+	$("#t-carousel-1 .current-slide .fd-pcard:first a").focus();
+
+}
+
+noTabDots = function(){
+
+	$(".multi-product .fd-carrousel__control__list a").attr("tabindex", -1);
+
+}
+
+//// find current slide and disable prev/next button
+$(document).ready(function() {
+
+	checkCurrent();
+	noTabDots();
+
+});
+
+//// focus first item in current carousel slide
+$(".carousel-top .header-btn").click(function() {
+
+	focusFirstEl();
+
+});
+
+//// previous/next [ENTER] key behavior
+$(document).ready(function() {
+ 
+  // previous button logic
+  $(".fd-carrousel__button__previous button").keyup(function(e) {
+   
+    var code = e.keyCode || e.which;
+       
+    if (code === 13) {
+      
+      //console.log('left Tab pressed');
+      setTimeout(function(){ $(".current-slide a:last").focus();}, 500);
+ 
+    }
+ 
+  });
+ 
+  // next button logic
+  $(".fd-carrousel__button__next button").keyup(function(e) {
+  
+    var code = e.keyCode || e.which;  
+ 
+    if (code === 13) {
+      
+      //console.log('right Tab pressed');
+      setTimeout(function(){ $(".current-slide a:first").focus();}, 500);
+    
+    }
+   
+  }); 
+ 
 });
 
